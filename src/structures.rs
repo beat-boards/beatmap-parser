cf5f906334d3c11ffb7f721ba9acd1f9de8e6cdd
Raphaël Thériault <raphael_theriault@outlook.com>
2019-07-25 13:56:27 -0400

    Refactor

--- src/structures.rs
+++ src/structures.rs
@@ -4,81 +4,88 @@ extern crate semver;
 extern crate serde;
 
 use semver::Version;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
-pub enum Environment {
-    DefaultEnvironment,
-    BigMirrorEnvironment,
-    TriangleEnvironment,
-    NiceEnvironment,
-    KDAEnvironment,
-    MonstercatEnvironment,
-}
+pub mod beatmap {
+    use super::{Deserialize, Serialize};
 
-pub mod custom_data {
-    use serde::{Serialize, Deserialize};
-
-    #[derive(Serialize, Deserialize)]
-    pub struct Contributor {
-        pub role: String,
-        pub name: String,
-        pub icon_path: String,
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct CustomData {
-    pub contributors: Vec<custom_data::Contributor>,
-    pub custom_environment: String,
-    pub custom_environment_hash: String,
-}
-
-pub mod difficulty_beatmap_set {
-    use serde::{Serialize, Deserialize};
-
-    #[derive(Serialize, Deserialize)]
-    pub enum BeatmapCharacteritic {
-        Standard,
-        NoArrows,
-        OneSaber,
-        Lawless,
-        Lightshow,
+    #[derive(Serialize, Deserialize, Debug)]
+    pub enum Environment {
+        DefaultEnvironment,
+        BigMirrorEnvironment,
+        TriangleEnvironment,
+        NiceEnvironment,
+        KDAEnvironment,
+        MonstercatEnvironment,
     }
 
-    #[derive(Serialize, Deserialize)]
-    pub enum Difficulty {
-        Easy,
-        Normal,
-        Hard,
-        Expert,
-        ExpertPlus,
+    pub mod custom_data {
+        use super::{Deserialize, Serialize};
+
+        #[derive(Serialize, Deserialize, Debug)]
+        pub struct Contributor {
+            pub role: String,
+            pub name: String,
+            pub icon_path: String,
+        }
     }
 
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize, Deserialize, Debug)]
     pub struct CustomData {
-        pub difficulty_label: String,
-        
+        pub contributors: Vec<custom_data::Contributor>,
+        pub custom_environment: String,
+        pub custom_environment_hash: String,
     }
 
-    #[derive(Serialize, Deserialize)]
-    pub struct DifficultyBeatmap {
-        pub difficulty: Difficulty,
-        pub difficulty_rank: u32,
-        pub beatmap_filename: String,
-        pub note_jump_movement_speed: f64,
-        pub note_jump_start_beat_offset: f64,
-        pub custom_data: CustomData
+    pub mod difficulty_beatmap_set {
+        use super::{Deserialize, Serialize};
+
+        #[derive(Serialize, Deserialize, Debug)]
+        pub enum BeatmapCharacteritic {
+            Standard,
+            NoArrows,
+            OneSaber,
+            Lawless,
+            Lightshow,
+        }
+
+        pub mod difficulty_beatmap {
+            use super::{Deserialize, Serialize};
+
+            #[derive(Serialize, Deserialize, Debug)]
+            pub enum Difficulty {
+                Easy,
+                Normal,
+                Hard,
+                Expert,
+                ExpertPlus,
+            }
+
+            #[derive(Serialize, Deserialize, Debug)]
+            pub struct CustomData {
+                pub difficulty_label: String,
+            }
+        }
+
+        #[derive(Serialize, Deserialize, Debug)]
+        pub struct DifficultyBeatmap {
+            pub difficulty: difficulty_beatmap::Difficulty,
+            pub difficulty_rank: u32,
+            pub beatmap_filename: String,
+            pub note_jump_movement_speed: f64,
+            pub note_jump_start_beat_offset: f64,
+            pub custom_data: difficulty_beatmap::CustomData,
+        }
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct DifficultyBeatmapSet {
+        pub beatmap_characteristic_name: difficulty_beatmap_set::BeatmapCharacteritic,
+        pub difficulty_beatmaps: Vec<difficulty_beatmap_set::DifficultyBeatmap>,
     }
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct DifficultyBeatmapSet {
-    pub beatmap_characteristic_name: difficulty_beatmap_set::BeatmapCharacteritic,
-    pub difficulty_beatmaps: Vec<difficulty_beatmap_set::DifficultyBeatmap>
-}
-
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Beatmap {
     pub version: Version,
     pub song_name: String,
@@ -93,7 +100,7 @@ pub struct Beatmap {
     pub preview_duration: f64,
     pub song_filename: String,
     pub cover_image_filename: String,
-    pub environment_name: Environment,
-    pub custom_data: CustomData,
-    pub difficulty_beatmap_sets: Vec<DifficultyBeatmapSet>,
+    pub environment_name: beatmap::Environment,
+    pub custom_data: beatmap::CustomData,
+    pub difficulty_beatmap_sets: Vec<beatmap::DifficultyBeatmapSet>,
 }
